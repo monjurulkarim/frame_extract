33c6c9c942406d89539e9baef79b5c7d9226143f
Muhammad Monjurul Karim <40798690+monjurulkarim@users.noreply.github.com>
2022-01-22 19:25:45 -0500

    Initial commit

---
+++ extract.py
@@ -0,0 +1,11 @@
+from src.frame_extract import FrameExtract
+
+
+input_dir = './input/' #Directory that contains the videos, you
+                        #want to extract frames
+destination_dir = './output/' #destination directory where extracted frames
+                                # will be stored. For each video it will create
+                                # separate subfolder by each video's name
+dim = (224,224)
+
+FrameExtract(input_dir,destination_dir,dim)
