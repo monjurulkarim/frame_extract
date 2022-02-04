0796189342b7c1f8e001810eb30c559acb0a9cbe
Muhammad Monjurul Karim <40798690+monjurulkarim@users.noreply.github.com>
2022-02-03 20:24:27 -0500

    Minor bug fixed

--- src/frame_extract.py
+++ src/frame_extract.py
@@ -34,19 +34,22 @@ def FrameExtract(input_dir,destination_dir,*args):
                 cap.set(1,loop)
                 success = cap.grab()
                 ret, image = cap.retrieve()
-                if args:
-                    for arg in args:
-                        image = cv2.resize(image,arg)
+                try:
+                    if args:
+                        for arg in args:
+                            image = cv2.resize(image,arg)
 
-                frame_name = vname_slice[0] +'_frame_%d.jpg' %loop
-                images.append(frame_name)
-                saved_path = '/'+ vname_slice[0]
-                destination_2 = destination_dir + saved_path
-                if not os.path.exists(destination_2):
-                    os.makedirs(destination_2)
+                    frame_name = vname_slice[0] +'_frame_%d.jpg' %loop
+                    images.append(frame_name)
+                    saved_path = '/'+ vname_slice[0]
+                    destination_2 = destination_dir + saved_path
+                    if not os.path.exists(destination_2):
+                        os.makedirs(destination_2)
 
-                saved_dir = osp.join(destination_2, frame_name)
-                cv2.imwrite(saved_dir,image)
+                    saved_dir = osp.join(destination_2, frame_name)
+                    cv2.imwrite(saved_dir,image)
+                except:
+                    continue
         count = count + 1
         cap.release()
         cv2.destroyAllWindows()
